bc4aba73fa75bb7a8f67130c26a25b3a71c853d7
Omri Levy <omri@kikivp.com>
2022-12-20 08:33:47 +0200

    set screen boundaries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,29 +27,7 @@ public class Player : MonoBehaviour
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
         
         transform.Translate(direction * (_walkingSpeed * Time.deltaTime));
-        
-        // wrap player movment
-        if (transform.position.y >= 6.6f)
-        {
-            transform.position = new Vector3(transform.position.x, -6.6f, 0);
-        }
-        else if (transform.position.y <= -6.6f)
-        {
-            transform.position = new Vector3(transform.position.x, 6.6f, 0);   
-        }
-        if (transform.position.x >= 14.3)
-        {
-            transform.position = new Vector3(-14.3f, transform.position.y, 0);
-        }
-        else if (transform.position.x <= -14.3f)
-        {
-            transform.position = new Vector3(14.3f, transform.position.y, 0);   
-        }
-        
-        /*
-         * limit user movement
-         *  transform.position = new Vector3(transform.position.x, Mathf.clamp(transform.position.y,-6,6), 0); 
-         *  transform.position = new Vector3(Mathf.clamp(transform.position.x,-13.5,13.5),transform.position.y , 0); 
-         */
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y,-5f,5f), 0);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x,-9.25f,9.25f),transform.position.y,0);
     }
 }
