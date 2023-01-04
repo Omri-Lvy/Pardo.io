02c410ed804b675de9228e540f6ba0a40e8369ab
Omri Levy <omri@kikivp.com>
2023-01-04 13:18:12 +0200

    enemy animations

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -11,11 +11,11 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float _def = 10;
     [SerializeField] private float _health = 100;
     [SerializeField] private int _xpGiven = 10;
-    [SerializeField] public Animator animator;
+    [SerializeField] public Animator _animator;
     private PlayerStats _playerStats;
     private EnemyStats _stats;
     private float timer;
-    private float timerLimit = 0.10f;
+    private float timerLimit = 0.25f;
 
 
     // Start is called before the first frame update
@@ -27,6 +27,32 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(_stats.getIsHit());
+        if (_stats.isDead())
+        {
+            _player.GetComponent<Player>().getStats().addXp(_xpGiven);
+            Debug.Log("XP: " + _player.GetComponent<Player>().getStats().getXp());
+            _animator.Play("Snail_die");
+            Destroy(gameObject,1f);
+        }
+        else if (_stats.getIsHit() && !_stats.isDead())
+        {
+            if (timer < timerLimit)
+            {
+                timer += 1 * Time.deltaTime;
+                _stats.setSpeed(0);
+            }
+            else
+            {
+                _stats.setIsHit(false);
+                timer = 0;
+            }
+        }
+        else if (!_stats.getIsHit() && !_stats.isDead())
+        {
+            _stats.setSpeed(_speed);
+            _animator.Play("Snail_walk");
+        }
         if (_player.transform.position.x > transform.position.x)
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
@@ -36,34 +62,22 @@ public class Enemy : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         transform.position = Vector2.MoveTowards(this.transform.position, _player.transform.position, _stats.getSpeed() * Time.deltaTime);
-        if (_stats.isDead())
-        {
-            _player.GetComponent<Player>().getStats().addXp(_xpGiven);
-            Debug.Log("XP: " + _player.GetComponent<Player>().getStats().getXp());
-            Destroy(gameObject);
-        }
-        if (_stats.getIsHit())
-        {
-            animator.SetBool("hit", true);
-            timer += Time.deltaTime;
-            _stats.setSpeed(0);
-            if (timer > timerLimit)
-            {
-                _stats.setIsHit(false);
-                timer = 0;
-            }
-        }
-        if (!_stats.getIsHit())
-        {
-            _stats.setSpeed(_speed);
-            animator.SetBool("hit", false);
-        }
-
     }
 
     public EnemyStats getStats()
     {
         return _stats;
+    }
+
+    public void getHit(float damage)
+    {
+        if (!_stats.isDead())
+        {
+            _stats.getHit(damage);
+            timer = 0;
+            _stats.setSpeed(0);
+            _animator.Play("Snail_hit");
+        }
     }
 
 }
